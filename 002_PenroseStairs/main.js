648604c957ff4f2902b0b24aa55f1942217ef2e4
hikari fujioka <h.fujioka@cec-ltd.co.jp>
2015-10-22 12:53:25 +0900

    002_PenroseStairsの完成

--- 002_PenroseStairs/main.js
+++ 002_PenroseStairs/main.js
@@ -14,44 +14,23 @@ window.onload = function() {
         this.frame = 32;
 
         this.image = core.assets['chara1.png'];
-        var straightCount = 0;
-        this.moveStraight = function(x, y, frame) {
-          this.tl.moveBy(x, y, frame)
-            .moveBy(x * -1, y * -1, frame * 2)
-            .repeat(function(){
-              if (4 < straightCount) {
-                this.tl.unloop();
-              }
-              straightCount++;
-            }, 5)
-            .loop();
-
-        }
 
         var drad = Math.PI * 2 / 100;
         var theta = 0;
         var r = 30;
-        this.moveTurnAround = function(frame) {
-          this.on('enterframe', function(){
-            theta += 0.8;
-            var x = r * Math.cos(theta);
-            var y = r * Math.sin(theta);
-
-            this.tl.moveBy(x, y, frame);
-          });
+        this.moveAround = function(frame) {
+          this.tl.moveBy(125, -35, frame)
+          .moveBy(48, 35, frame)
+          .moveBy(-48, 35, frame)
+          .moveTo(this.x, this.y, frame)
+          .loop();
         }
-
         core.rootScene.addChild(this);
       }
     });
 
-    var straightBear = new Bear(10, 80);
-    straightBear.moveStraight(0, 140, 20);
-
-
-    var turnaroundBear = new Bear(100, 80);
-    turnaroundBear.moveTurnAround(10);
-
+    var bear = new Bear(15, 78);
+    bear.moveAround(10);
   }
   core.start();
 };
